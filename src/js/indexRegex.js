0663986a8c5b092cc6e2fbffda4a5b7ab0b6b4c8
Gustavo Tamanini <gustavotamanini2005@gmail.com>
2025-09-24 20:28:10 -0300

    feature: código regex do index refatorado

--- src/js/indexRegex.js
+++ src/js/indexRegex.js
@@ -3,27 +3,34 @@ const btnEnviar = document.querySelector("#btn_enviar");
 const cxNome = document.querySelector("#nome");
 const cxEmail = document.querySelector("#email");
 
+const flashMessageNome = document.querySelector("#nome_vazio");
+const flashMessageEmail = document.querySelector("#email_invalido");
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
 cxNome.addEventListener("blur", function(e){
-    const flashMessageNome = document.querySelector("#nome_vazio");
-    if(cxNome.value == ""){
-        flashMessageNome.style.display = "block";
-    }else{
-        flashMessageNome.style.display = "none";
-    }
+    flashMessageNome.style.display = !cxNome.value == "" ? "none" : "block";
 })
 
-cxEmail.addEventListener("blur", function(e){
-    const flashMessageEmail = document.querySelector("#email_invalido");
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
-    if(!emailRegex.test(cxEmail.value)){
-        flashMessageEmail.style.display = "block";
-    }else{
-        flashMessageEmail.style.display = "none";
-    }
+cxEmail.addEventListener("blur", function(e){ 
+    
+    flashMessageEmail.style.display = emailRegex.test(cxEmail.value) ? "none" : "block";
 })
 
 btnEnviar.addEventListener('click', function(e){
-    const flashMessageSucesso = document.querySelector("#mensagem_sucesso");
-    flashMessageSucesso.style.display = "block";
-    e.preventDefault();
+    if(cxNome.value != "" && emailRegex.test(cxEmail.value)){
+        const flashMessageSucesso = document.querySelector("#mensagem_sucesso");
+        flashMessageSucesso.style.display = "block";
+        setTimeout(() => {
+            flashMessageSucesso.style.display = "none";
+        }, 5000)
+        e.preventDefault();
+    }else{
+        const flashMessageErro = document.querySelector("#mensagem_erro");
+        flashMessageErro.style.display = "block";
+        setTimeout(() => {
+            flashMessageErro.style.display = "none";
+        }, 3000)
+        e.preventDefault();
+    }
 })
